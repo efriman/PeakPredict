aeea7fe7de9ecf5832154bc67cf7d47d21602128
efriman <elias.friman@gmail.com>
2023-05-26 16:11:40 +0100

    Added function to run predictive models and plot output

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # overlap_peak_tables
-Simple script to count the number of overlapping or closest features between one and multiple bed files
+Simple script to count the number of overlapping or closest features between one and multiple bed files.
+It can also run models to make predictions of a feature based on these overlaps.
 
 ## Installation
 `git clone https://github.com/efriman/overlap_peak_tables.git`
@@ -13,9 +14,34 @@ Simple script to count the number of overlapping or closest features between one
 
 overlap_peak_tables takes a base bed which you want to overlap multiple other bedfiles with (`--overlap_features`). BED files need to either be without headers or contain at least the headers `chrom`, `start`, and `end`. By default, it quantifies the number of peaks overlapping each peak in the base_bed (0/1/2 etc). If you prefer you can set `--boolean_output` to get True/False overlaps. You can also run it with `--closest` to get the number of peaks in vicinity of the peak (parameters for this are `--k`, `--mindist`, and `--maxdist`).
 
+To run predictions based on the peak overlaps, set `--prediction_column` to the column in the base_bed you want to predict. Other options for the prediction are `--model`, `--test_size`, `--seed`, and `--plot_size`. By default, the prediction is done using LogisticRegression with `test_size` 0.3 (30%/70% test/training split) without a set seed. Outputs from predictions are:
+-Correlation matrix between predictors and feature to be predicted
+-Table including predictions for the test data
+-Confusion matrix showing true/predicted groups in the test data
+-Feature importance graph (For some models, such as GaussianNB, this is done using permutation test, which can take time)
+
+Available models are the following (from scikit-learn, https://scikit-learn.org/):
+
+LogisticRegression
+SVC
+GaussianNB
+MultinomialNB
+SGDClassifier
+KNeighborsClassifier
+DecisionTreeClassifier
+RandomForestClassifier
+GradientBoostingClassifier
+LinearRegression
+SGDRegressor
+KernelRidge
+ElasticNet
+BayesianRidge
+GradientBoostingRegressor
+SVR
+
 
 ## Inputs and outputs (examples)
-`overlap_peak_tables base_bed.bed --overlap_features overlap_features.bed --output outfile.bed`
+`overlap_peak_tables base_bed.bed --overlap_features overlap_features.bed --output outfile`
 
 base_bed.bed
 | chrom  | start | end |
@@ -24,15 +50,22 @@ base_bed.bed
 | chr1  | 1000  | 2000 |
 | chr1  | 5000  | 5300 |
 
-overlap_features.bed
+overlap_features1.bed
 | chrom  | start | end |
 | ------------- | ------------- | ------------- |
 | chr1  | 5  | 50 |
 | chr1  | 5100  | 5500 |
 
-outfile.bed
-| chrom  | start | end | overlap_features.bed |
-| ------------- | ------------- | ------------- | ------------- |
-| chr1  | 1  | 100 | 1 |
-| chr1  | 1000  | 2000 | 0 |
-| chr1  | 5000  | 5300 | 1 |
+overlap_features2.bed
+| chrom  | start | end |
+| ------------- | ------------- | ------------- |
+| chr1  | 10  | 20 |
+| chr1  | 1200  | 1300 |
+
+outfile.tsv
+| chrom  | start | end | overlap_features1.bed | overlap_features2.bed |
+| ------------- | ------------- | ------------- | ------------- | ------------- |
+| chr1  | 1  | 100 | 1 | 1 |
+| chr1  | 1000  | 2000 | 0 | 1 |
+| chr1  | 5000  | 5300 | 1 | 0 |
+
--- overlap_peak_tables/overlap_peak_tables.py
+++ overlap_peak_tables/overlap_peak_tables.py
@@ -6,6 +6,11 @@ from .lib.utils import sniff_for_header
 import logging
 import warnings
 import argparse
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import matthews_corrcoef, ConfusionMatrixDisplay
+from sklearn.inspection import permutation_importance
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 warnings.filterwarnings(action="ignore", message=r".*index_col*")
 logging.basicConfig(format="%(message)s", level='INFO')
@@ -61,7 +66,51 @@ def count_closest(base_features, overlap_bed_file, chr_or_no, k=100, mindist=1, 
     features = pd.merge(base_features, count_closest, on="coords", how="left").fillna(0)
     features[overlap_bed_file] = features[overlap_bed_file].astype(int)
     return features
-    
+
+def extract_test_train(input_table, predict_column, predictors_columns, test_size=0.3, random_state=None):
+    X = input_table[predictors_columns]
+    y = input_table[predict_column]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state, shuffle=True)
+    logging.info(f"Training Data Shape: {X_train.shape}")
+    logging.info(f"Testing Data Shape: {X_test.shape}")
+    return X_train, X_test, y_train, y_test
+
+def correlate_features(X, y):
+    train_df = pd.concat([X, y], axis=1)
+    corr_matrix = train_df.corr()
+    return corr_matrix
+
+def model_predict(X_train, X_test, y_train, y_test, model):
+    model = model()
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    mcc = matthews_corrcoef(y_test, y_pred)
+    logging.info(f"Matthew's correlation coefficient: {mcc}")
+    return y_pred, model
+
+def extract_feature_importance(model, X, y):
+    skip_importance = False
+    try:
+        coefficients = model.coef_
+        avg_importance = np.mean(np.abs(coefficients), axis=0)
+    except AttributeError:
+        try:
+            coefficients = model.feature_importances_
+            avg_importance = coefficients
+        except AttributeError:
+            logging.info("Calculating feature importance using permutation (can be slow, Ctrl+C to skip this step)")
+            coefficients = permutation_importance(model, X, y)
+            avg_importance = coefficients["importances_mean"]
+        except:
+            warnings.warn("Can't calculate feature importances using this model")
+            skip_importance = True
+    if skip_importance:
+        pd.DataFrame({'Feature': X.columns, 'Importance': np.repeat(np.nan, X.shape[1])})
+    else:
+        feature_importance = pd.DataFrame({'Feature': X.columns, 'Importance': avg_importance})
+        feature_importance = feature_importance.sort_values('Importance', ascending=True)
+    return feature_importance
+
     
 def parse_args_overlap_count_tables():
     parser = argparse.ArgumentParser(
@@ -86,7 +135,7 @@ def parse_args_overlap_count_tables():
         "--o",
         type=str,
         required=True,
-        help="""Where to save output file""",
+        help="""Prefix for output files. Output table will be saved as .tsv""",
     )
     parser.add_argument(
         "--boolean_output",
@@ -124,6 +173,58 @@ def parse_args_overlap_count_tables():
         default=1_000_000,
         help="""Maximum distance for the number of closest peaks""",
     )
+    parser.add_argument(
+        "--predict_column",
+        "--predict-column",
+        type=str,
+        required=False,
+        default=None,
+        help="""The name of the column to predict based on features used in the overlap""",
+    )
+    parser.add_argument(
+        "--model",
+        type=str,
+        required=False,
+        default="LogisticRegression",
+        help="""The name of the model used for prediction. Available models are:
+        LogisticRegression
+        SVC
+        GaussianNB
+        MultinomialNB
+        SGDClassifier
+        KNeighborsClassifier
+        DecisionTreeClassifier
+        RandomForestClassifier
+        GradientBoostingClassifier
+        LinearRegression
+        SGDRegressor
+        KernelRidge
+        ElasticNet
+        BayesianRidge
+        GradientBoostingRegressor
+        SVR
+        """,
+    )
+    parser.add_argument(
+        "--test_size",
+        type=int,
+        required=False,
+        default=0.3,
+        help="""The fraction of dataset to be split for testing (and the rest for training)""",
+    )
+    parser.add_argument(
+        "--seed",
+        type=int,
+        required=False,
+        help="""Seed used for random splitting data for prediction. Set for reproducibility""",
+    )
+    parser.add_argument(
+        "--plot_size",
+        type=int,
+        default=1,
+        required=False,
+        help="""Relative size of plots. Adjust if they don't look right (too many/few features)""",
+    )
 
     return parser
 
@@ -143,27 +244,104 @@ def main():
                                      dtypes=dtypes,
                                      coord_column=True)
     
+    overlap_table = base_peaks.copy()
+    
     if args.closest:
         for overlap_feature in args.overlap_features:
             logging.info(f"Counting the number of overlapping features with {overlap_feature} within {args.mindist} and {args.maxdist} bp (up to {args.k} allowed, change with --k)")
-            base_peaks = count_closest(base_peaks, 
-                                       overlap_feature, 
-                                       chr_or_no=chr_or_no,
-                                       k=args.k, 
-                                       mindist=args.mindist, 
-                                       maxdist=args.maxdist)
+            overlap_table = count_closest(overlap_table, 
+                                          overlap_feature, 
+                                          chr_or_no=chr_or_no,
+                                          k=args.k, 
+                                          mindist=args.mindist, 
+                                          maxdist=args.maxdist)
     else:
         for overlap_feature in args.overlap_features:
             logging.info(f"Checking overlaps with {overlap_feature}")
-            base_peaks = count_overlaps(base_peaks, 
-                                        overlap_feature,
-                                        chr_or_no=chr_or_no,
-                                        boolean_output=args.boolean_output)
-    
-    base_peaks.drop(columns="coords").to_csv(args.output, sep="\t", index=False, header=True)
-    
-    logging.info(f"Saved output file as {args.output}")
+            overlap_table = count_overlaps(overlap_table, 
+                                           overlap_feature,
+                                           chr_or_no=chr_or_no,
+                                           boolean_output=args.boolean_output)
 
+    overlap_table.drop(columns="coords").to_csv(f"{args.output}.tsv", sep="\t", index=False, header=True)
+    
+    logging.info(f"Saved overlap table as {args.output}.tsv")
+    
+    if args.predict_column is not None:
+        if args.predict_column not in overlap_table:
+            raise ValueError(f"column {args.predict_column} doesn't exist in the base bed")
+        if args.seed:
+            random_state = args.seed
+        else:
+            random_state = None
+        predictors_columns = [col for col in list(overlap_table.columns) if col not in (base_peaks.columns)]
+        X_train, X_test, y_train, y_test = extract_test_train(overlap_table, 
+                                                              args.predict_column, 
+                                                              predictors_columns, 
+                                                              test_size=args.test_size, 
+                                                              random_state=random_state)
+        logging.info(f"Plotting correlation matrix as {args.output}.png")
+        corr_matrix = correlate_features(X_train, y_train)
+        g = sns.clustermap(corr_matrix, cmap="coolwarm", vmin=-1, vmax=1, 
+                           yticklabels=True, xticklabels=True,
+                           figsize=(args.plot_size,args.plot_size))
+        g.savefig(f"corr_features_{args.output}.png", dpi=100)
+        
+        from sklearn.linear_model import LogisticRegression
+        from sklearn.svm import SVC
+        from sklearn.naive_bayes import GaussianNB
+        from sklearn.naive_bayes import MultinomialNB
+        from sklearn.linear_model import SGDClassifier
+        from sklearn.neighbors import KNeighborsClassifier
+        from sklearn.tree import DecisionTreeClassifier
+        from sklearn.ensemble import RandomForestClassifier
+        from sklearn.ensemble import GradientBoostingClassifier
+        from sklearn.linear_model import LinearRegression
+        from sklearn.linear_model import SGDRegressor
+        from sklearn.kernel_ridge import KernelRidge
+        from sklearn.linear_model import ElasticNet
+        from sklearn.linear_model import BayesianRidge
+        from sklearn.ensemble import GradientBoostingRegressor
+        from sklearn.svm import SVR
+        
+        model_dict = {"LogisticRegression":LogisticRegression,
+                "SVC":SVC,
+                "GaussianNB":GaussianNB,
+                "MultinomialNB":MultinomialNB,
+                "SGDClassifier":SGDClassifier,
+                "KNeighborsClassifier":KNeighborsClassifier,
+                "DecisionTreeClassifier":DecisionTreeClassifier,
+                "RandomForestClassifier":RandomForestClassifier,
+                "GradientBoostingClassifier":GradientBoostingClassifier,
+                "LinearRegression":LinearRegression,
+                "SGDRegressor":SGDRegressor,
+                "KernelRidge":KernelRidge,
+                "ElasticNet":ElasticNet,
+                "BayesianRidge":BayesianRidge,
+                "GradientBoostingRegressor":GradientBoostingRegressor,
+                "SVR":SVR,}
+
+        if args.model not in model_dict.keys():
+            raise ValueError(f"{args.model} is not available")
+
+        logging.info(f"Predicting {args.predict_column} based on overlap counts using {args.model}")
+        y_pred, model_fit = model_predict(X_train, X_test, y_train, y_test, model=model_dict[args.model])
+        predictions = X_test.join(base_peaks).reset_index(drop=True)
+        predictions[f"{args.predict_column}_pred_{args.model}"] = y_pred 
+        predictions.to_csv(f"{args.output}_predict_{args.predict_column}_{args.model}.tsv", sep="\t", index=False, header=True)
+        logging.info(f"Saved predictions of test data as {args.output}_predict_{args.predict_column}_{args.model}.tsv")
+        
+        logging.info(f"Plotting confusion matrix as {args.output}_confusion_matrix_{args.predict_column}_{args.model}.png")
+        ConfusionMatrixDisplay.from_predictions(y_test, y_pred)
+        plt.tight_layout()
+        plt.savefig(f"{args.output}_confusion_matrix_{args.predict_column}_{args.model}.png", dpi=300, bbox_inches="tight")
+        
+        logging.info(f"Plotting feature importance as {args.output}_{args.model}_feature_importance.png")
+        feature_importance = extract_feature_importance(model_fit, X_test, y_test)
+        plt.figure(figsize=(args.plot_size,args.plot_size))
+        sns.barplot(data=feature_importance, x='Feature', y='Importance', color="skyblue", edgecolor="black")
+        plt.xticks(rotation=45, ha="right")
+        plt.savefig(f"{args.output}_{args.model}_feature_importance.png", dpi=300, bbox_inches="tight")
 
 if __name__ == '__main__':
     main()
